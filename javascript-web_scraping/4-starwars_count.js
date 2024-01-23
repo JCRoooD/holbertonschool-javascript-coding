cf4d83fff93c153a72b28b19498fd06100d5de67
JCRoooD <juancarlos-99@live.com>
2024-01-23 16:07:45 -0400

    wedge antilles fix checkers

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,24 @@
 #!/usr/bin/node
 /* Script that prints the number of movies where the character “Wedge Antilles” is present. */
 
-const characterID = '18';
-const url = process.argv[2];
 const request = require('request');
+const url = process.argv[2];
 
 request(url, (err, response, body) => {
-
   if (err) {
     console.error('Error:', err);
     return;
   }
 
-  const data = JSON.parse(body);
   let count = 0;
+  const films = JSON.parse(body).results;
 
-  for (const film of data.results) {
-    for (const character of film.characters) {
-      if (character.endsWith(characterID)) {
+  films.forEach((film) => {
+    film.characters.forEach((character) => {
+      if (character.includes('18')) {
         count++;
       }
-    }
-  }
+    });
+  });
   console.log(count);
 });
