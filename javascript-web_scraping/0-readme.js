a7e47fb19f6cd127b79c94309bd01c7a8dd9a612
JCRoooD <juancarlos-99@live.com>
2024-01-23 12:06:50 -0400

    semi standard fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+/* Script that reads and prints the content of a file. */
+
 const fs = require('fs');
 
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data); 
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
