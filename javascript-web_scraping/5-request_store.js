5dd97230472c429211b63e1aa360d35058a9ee56
JCRoooD <juancarlos-99@live.com>
2024-01-23 18:35:25 -0400

    i cant see the checkers!

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,13 +2,17 @@
 /* Script that writes a string to a file. */
 const request = require('request');
 const fs = require('fs');
+
 const url = process.argv[2];
+const filePath = process.argv[3];
 
 request(url, (err, response, body) => {
   if (err) {
-    console.error(err);
+    console.error('Error:', err);
+    return;
   }
-  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+
+  fs.writeFile(filePath, body, 'utf8', (err) => {
     if (err) {
       console.error(err);
     }
