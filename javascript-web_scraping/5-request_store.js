fc7890e554fb1fd1a1ff3ab81b137ca98f53265b
JCRoooD <juancarlos-99@live.com>
2024-01-24 10:53:39 -0400

    fixing output

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-/* Script that writes a string to a file. */
 const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (err, response, body) => {
-  if (err) {
-    console.error('Error:', err);
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
     return;
   }
 
